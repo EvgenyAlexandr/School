be6ebf6d199ca24d89ea623b3c4d9027d357838f
EvgenyAlexand <bragin-e@mail.ru>
2025-11-30 03:08:24 +0700

    Базы данных - Тестирование - Уборка мусора.

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -2,9 +2,6 @@ package ru.hogwarts.school.service;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.*;
@@ -16,78 +13,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.*;
-import java.util.Collection;
 
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
-
-//@Service
-//public class StudentService {
-//
-//    @Value("${avatars.dir.path}")
-//    private String avatarsDir;
-//
-//    private final StudentRepository studentRepository;
-//    private final AvatarRepository avatarRepository;
-//
-//    public StudentService(StudentRepository studentRepository, AvatarRepository avatarRepository) {
-//        this.studentRepository = studentRepository;
-//        this.avatarRepository = avatarRepository;
-//    }
-//
-//    public Student addStudent(Student student) {
-//        student.setId(null);
-//        return studentRepository.save(student);
-//    }
-//
-//    public Student findStudent(long id) {
-//        return studentRepository.findById(id).orElseThrow();
-//    }
-//
-//    public Student editStudent(Student student) {
-//        return studentRepository.save(student);
-//    }
-//
-//    public void deleteStudent(long id) {
-//        studentRepository.deleteById(id);
-//    }
-//
-//    public Collection<Student> findByAge(int age) {
-//        return studentRepository.findAllByAge(age);
-//    }
-//
-//    public Avatar findAvatar(long studentId) {
-//        return avatarRepository.findByStudentId(studentId).orElseThrow();
-//    }
-//
-//    public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
-//        Student student = findStudent(studentId);
-//
-//        Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(file.getOriginalFilename()));
-//        Files.createDirectories(filePath.getParent());
-//        Files.deleteIfExists(filePath);
-//
-//        try (InputStream is = file.getInputStream();
-//             OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
-//             BufferedInputStream bis = new BufferedInputStream(is, 1024);
-//             BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
-//        ) {
-//            bis.transferTo(bos);
-//        }
-//
-//        Avatar avatar = avatarRepository.findByStudentId(studentId).orElseGet(Avatar::new);
-//        avatar.setStudent(student);
-//        avatar.setFilePath(filePath.toString());
-//        avatar.setFileSize(file.getSize());
-//        avatar.setMediaType(file.getContentType());
-//        avatar.setData(file.getBytes());
-//
-//        avatarRepository.save(avatar);
-//    }
-//
-//    private String getExtension(String fileName) {
-//        return fileName.substring(fileName.lastIndexOf(".") + 1);
-//    }
-//}
 
 
 @Service
@@ -106,45 +33,6 @@ public class AvatarService {
         this.studentRepository = studentRepository;
     }
 
-
-    // Отображение Аватар студента
-//    public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
-//        Student student = studentRepository.getById(studentId);
-//        Path filePath = Path.of(avatarsDir, student + "." + getExtensions(avatarFile.getOriginalFilename()));
-//        Files.createDirectories(filePath.getParent());
-//        Files.deleteIfExists(filePath);
-//        try (
-//                InputStream is = avatarFile.getInputStream();
-//                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
-//                BufferedInputStream bis = new BufferedInputStream(is, 1024);
-//                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
-//        ) {
-//            bis.transferTo(bos);
-//        }
-//        Avatar avatar = findAvatar(studentId);
-//        avatar.setStudent(student);
-//        avatar.setFilePath(filePath.toString());
-//        avatar.setFileSize(avatarFile.getSize());
-//        avatar.setMediaType(avatarFile.getContentType());
-//        avatar.setData(avatarFile.getBytes());
-//        avatarRepository.save(avatar);
-//    }
-
-//    // Поиск Аватар по ID студента
-//    public Avatar findAvatar(Long studentId) {
-//        return avatarRepository.findAvatarById(studentId).orElse(new Avatar());
-//    }
-//
-//    // Получит расширение файла (что за файл, как с ним работать)
-//    private String getExtensions(String fileName) {
-//        return fileName.substring(fileName.lastIndexOf(".") + 1);
-//    }
-//
-//    // Все Аватар
-//    public Page<Avatar> getAllAvatars(int page, int size) {
-//        Pageable pageable = PageRequest.of(page, size);
-//        return avatarRepository.findAll(pageable);
-//    }
 
     // Загрузка Аватар студента из общей папки
     public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
@@ -198,8 +86,4 @@ public class AvatarService {
     private String getExtension(String fileName) {
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
-
-
-
-
 }
