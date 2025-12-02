7ef7efcc84cf18b3d1a99c69d52b52f553db08f4
EvgenyAlexand <bragin-e@mail.ru>
2025-12-03 02:47:58 +0700

    Базы данных - Миграции и индексы - Все задания Выполнены.

---
+++ src/main/resources/liquibase/scripts/index-praktik.sql
@@ -0,0 +1,7 @@
+-- liquibase formatted sql
+
+-- changeset evgeniy:create_student_name_index
+CREATE INDEX idx_student_name ON student(name);
+
+-- changeset evgeniy:create_faculty_name_color_index
+CREATE INDEX idx_faculty_name_color ON faculty(name, color);
