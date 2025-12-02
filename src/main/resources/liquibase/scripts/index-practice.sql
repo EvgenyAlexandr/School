87a3e215e4ff014135292c1caa6710768916fc9b
EvgenyAlexand <bragin-e@mail.ru>
2025-12-03 03:07:29 +0700

    Базы данных - Миграции и индексы - Все задания Выполнены.

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 
--- changeset evgeniy:1 create_student_name_index
+-- changeset evgeniy:create_student_name_index
 CREATE INDEX idx_student_name ON student(name);
 
--- changeset evgeniy:2 create_faculty_name_color_index
+-- changeset evgeniy:create_faculty_name_color_index
 CREATE INDEX idx_faculty_name_color ON faculty(name, color);
