0dd2f5092b80d97e8e7e3dbc7f5b9e38bfff66b7
EvgenyAlexand <bragin-e@mail.ru>
2025-12-07 08:19:27 +0700

    Базы данных - Логирование и Конфигурация - В процессе.

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "InfoController", description = "Информация о Релизе и Порте работы")
 public class InfoController {
 
     private static final Logger logger = LoggerFactory.getLogger(InfoController.class);
@@ -18,6 +21,7 @@ public class InfoController {
     private String activeProfile;
 
     @GetMapping("/port")
+    @Operation(summary = "Тип релиза и Порт")
     public String getPort() {
         logger.info("InfoController. Активный профиль: {}, Port: {}", activeProfile, serverPort);
         return "Active profile: " + activeProfile + ", Server port: " + serverPort;
